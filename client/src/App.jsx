2b7a3565677031e2830fd40b6f63205cd7e27331
Ahmed Eltohfa <eltohfa01@gmail.com>
2025-07-02 07:48:42 +0300

    creating pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav';
+import './App.css'
+import Home from './pages/Home.jsx';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import Login from './pages/Login';
+import Social from './pages/Social';
+import Rules from './pages/Rules';
+import Profile from './pages/Profile';
+import Waiting from './pages/Waiting';
+import Settings from './pages/Settings';
+import LeaderBoard from './pages/LeaderBoard';
+import Game from './pages/Game';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <Nav />
-    <div className='contain mx-2 md:mx-4'>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
-      <div className=''>hello</div>
+    <div className='contain mx-2 md:mx-4 lg:mx-10 xl:mx-12 mt-16 pt-1'>
+      <h1>hello</h1>
+      <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<Login />} />
+          <Route path="/social" element={<Social />} />
+          <Route path="/rules" element={<Rules />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/waiting" element={<Waiting />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/leaderboard" element={<LeaderBoard />} />
+          <Route path="/game" element={<Game />} />
+          <Route element={<NotFound />} />
+      </Routes>
     </div>
     </>
   )
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useState,useEffect, useRef } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import logo from '../assets/logo2.png';
 import profilePic from '../assets/avatars.png';
 import { FaHome, FaGamepad, FaTrophy, FaUserFriends, FaCog, FaUser, FaBars } from 'react-icons/fa';
@@ -7,24 +7,56 @@ import { FaHome, FaGamepad, FaTrophy, FaUserFriends, FaCog, FaUser, FaBars } fro
 const Navbar = () => {
     const location = useLocation();
     const [menuOpen, setMenuOpen] = useState(false);
+    const menuRef = useRef();
+    const navigate = useNavigate();
+
+    const handelProfileClick = () => {
+        navigate('/profile');
+    }
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (menuOpen && menuRef.current && !menuRef.current.contains(event.target)) {
+            setMenuOpen(false);
+            }
+        }
+        
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [menuOpen]);
+
+    useEffect(() => {
+    if (menuOpen) {
+        document.body.classList.add('overflow-hidden');
+        } else {
+        document.body.classList.remove('overflow-hidden');
+        }
+
+        return () => {
+        document.body.classList.remove('overflow-hidden');
+    };
+    }, [menuOpen]);
 
     const navItems = [
         { path: '/', name: 'Home', icon: <FaHome className="text-lg" /> },
         { path: '/game', name: 'Game', icon: <FaGamepad className="text-lg" /> },
         { path: '/leaderboard', name: 'Leaderboard', icon: <FaTrophy className="text-lg" /> },
-        { path: '/friends', name: 'Friends', icon: <FaUserFriends className="text-lg" /> },
+        { path: '/social', name: 'Friends', icon: <FaUserFriends className="text-lg" /> },
         { path: '/settings', name: 'Settings', icon: <FaCog className="text-lg" /> },
     ];
 
     return (
-        <nav className="fixed top-0 bottom-auto h-16 w-full z-50 bg-bg-main border-b border-gray-300 shadow-md flex items-center justify-between px-4">
+        <>
+        <nav className="fixed top-0 bottom-auto h-16 w-full z-50 bg-bg-main border-b border-gray-300 shadow-md flex items-center justify-between px-4 max-w-full overflow-hidden">
         {/* Left: Logo */}
-        <div className="flex w-[190px] overflow-hidden max-h-full p-3 items-center">
+        <div className="flex w-[190px] overflow-hidden max-h-full py-0 items-center">
             {/* Mobile Burger Icon */}
-            <div className="md:hidden order-0">
-            <button onClick={() => setMenuOpen(!menuOpen)} className="text-gray-300 hover:text-white text-xl">
-            <FaBars />
-            </button>
+            <div className="md:hidden order-0 flex items-center cursor-pointer">
+                <button onClick={() => setMenuOpen(!menuOpen)} className="text-gray-300 hover:text-white text-xl cursor-pointer">
+                <FaBars />
+                </button>
             </div>
             <img
             src={logo}
@@ -53,7 +85,7 @@ const Navbar = () => {
         </div>
 
         {/* Right: Profile Picture (Desktop) */}
-        <div className={`flex items-center`}>
+        <div className={`flex items-center cursor-pointer`} onClick={()=>{handelProfileClick()}}>
             <img
             src={profilePic}
             alt="Profile"
@@ -61,15 +93,17 @@ const Navbar = () => {
             />
         </div>
 
+        </nav>
         {/* Mobile Dropdown Menu */}
-        {menuOpen && (
-            <div className="absolute bottom-16 left-0 w-full bg-bg-main border-t border-gray-700 shadow-lg md:hidden flex flex-col items-start px-4 py-2 space-y-2">
-            {navItems.map((item) => (
+        { (
+            <div className={`fixed bg-gray-900 space-y-10 border-gray-700 shadow-lg flex flex-col items-center py-2 h-full justify-start left-0 top-0 ${menuOpen ? 'w-5/6 py-10 px-3' : 'w-0'} transition-all bg-gray-300 overflow-hidden z-[1000] gap-0`} ref={menuRef} > 
+            {navItems.map((item,index) => (
+                <>
                 <Link
                 key={item.path}
                 to={item.path}
                 onClick={() => setMenuOpen(false)}
-                className={`flex items-center space-x-2 text-sm w-full ${
+                className={`flex items-center space-x-2 text-sm w-full py-10 px-2 m-0 ${
                     location.pathname === item.path
                     ? 'text-blue-500'
                     : 'text-gray-300 hover:text-blue-400'
@@ -78,10 +112,14 @@ const Navbar = () => {
                 {item.icon}
                 <span>{item.name}</span>
                 </Link>
+                {index !== navItems.length - 1 && (
+                    <hr className="border-t border-gray-400 w-full m-0" />
+                )}
+                </>
             ))}
             </div>
         )}
-        </nav>
+        </>
     );
 };
 
